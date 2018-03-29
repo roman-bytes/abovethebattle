c69d1288fcb515a89fe230c48e3685043ca6565c
Jacob Roman <jacob.roman@deque.com>
2018-03-29 08:17:00 -0400

    fixed image issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 590,
+              maxWidth: 1590,
             },
           },
           {
