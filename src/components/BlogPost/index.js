197d73e4c89a85ef73cccabd766bfef8788bb2a7
Jacob Roman <jacob.roman@deque.com>
2018-03-28 22:34:47 -0400

    more homepage layout

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -3,94 +3,88 @@ import Link from 'gatsby-link';
 import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Diamond from '../Diamond';
 
 export default class BlogPost extends Component {
   render() {
     const { node, index } = this.props;
-    const featImage = node.featuredImage && ( <Img sizes={node.featuredImage.sizes} />);
+    // console.log('node', node);
+    const featImage = node.featuredImage && (
+      <Img
+        sizes={node.featuredImage.sizes}
+      />
+    );
     const postClasses = classNames('post', {
       'featured': index === 0,
       'small-featured': index < 4 && index !== 0,
+      'other': index > 4
     });
+    const categories = node.category
+      && Array.prototype.map.call(node.category, s => s.category).toString();
 
     return (
-      <div className={postClasses} >
+      <div className={postClasses}>
+        {index > 4 && featImage}
         {
-          index === 0
-            ? (
-              <div className='post-heading'>
-                <h1>{node.title}</h1>
-                <svg
-                  width='85'
-                  height='10'
-                  viewBox='0 0 85 10'
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='heading-line'
-                >
-                  <g id='Welcome' fill='none' fillRule='evenodd' strokeLinecap='square'>
-                    <g id='Home-Page' transform='translate(-175 -341)' stroke='#FB3053'>
-                      <g id='Group' transform='translate(175 341)'>
-                        <path
-                          d='M0.346153846,8.69230769 L8.6826424,1.28209564' id='Line-3'
-                        />
-                        <path
-                          d='M17.2652738,8.67362413 L8.77950127,0.813452374 L17.2652738,8.67362413 Z'
-                          id='Line-3'
-                        />
-                        <path
-                          d='M17.3461538,8.69230769 L25.6826424,1.28209564'
-                          id='Line-3'
-                        />
-                        <path
-                          d='M34.1781951,8.67362413 L25.6924226,0.813452374 L34.1781951,8.67362413 Z'
-                          id='Line-3'
-                        />
-                        <path
-                          d='M34.3461538,8.69230769 L42.6826424,1.28209564'
-                          id='Line-3'
-                        />
-                        <path
-                          d='M51.0911164,8.67362413 L42.6053439,0.813452374 L51.0911164,8.67362413 Z'
-                          id='Line-3'
-                        />
-                        <path
-                          d='M51.3461538,8.69230769 L59.6826424,1.28209564'
-                          id='Line-3'
-                        />
-                        <path
-                          d='M68.0040377,8.67362413 L59.5182652,0.813452374 L68.0040377,8.67362413 Z'
-                          id='Line-3'
-                        />
-                        <path
-                          d='M68.3461538,8.69230769 L76.6826424,1.28209564'
-                          id='Line-3'
-                        />
-                        <path
-                          d='M84.916959,8.67362413 L76.4311865,0.813452374 L84.916959,8.67362413 Z'
-                          id='Line-3'
-                        />
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-              </div>
-            )
-            : (<h2>{node.title}</h2>)
+          index > 4 && (
+            <div className='categories'>{categories}</div>
+          )
+        }
+        {
+          index > 4 && (
+            <h3>{node.title}</h3>
+          )
+        }
+        {
+          index > 4 && index !== 0 && (
+            <Link
+            to={node.slug}
+            className='read-more'
+            >
+            READ ARTICLE
+            </Link>
+          )
         }
         {
           index > 4 && (<p>{node.createdAt}</p>)
         }
+        {
+          index === 0
+            && (
+              <div className='post-heading'>
+                <h1>{node.title}</h1>
+                <Diamond />
+              </div>
+            )
+        }
+        {
+          index < 4 && index !== 0 && (
+            <div className='secondary-heading'>
+              <h2><Link to={node.slug}>{node.title}</Link></h2>
+              <Diamond />
+              <p>
+                Article about:
+                {categories}
+              </p>
+            </div>
+          )
+        }
         <div>
-          {index > 4 && featImage}
-          <div className='excerpt'>
-            {node.content.childMarkdownRemark.excerpt}
-          </div>
-          <Link
-            to={node.slug}
-            className='read-more'
-          >
-            READ ARTICLE
-          </Link>
+          {index < 1 && (
+            <div className='excerpt'>
+              {node.content.childMarkdownRemark.excerpt}
+            </div>
+          )}
+          {
+            index === 0 && (
+              <Link
+              to={node.slug}
+              className='read-more'
+              >
+              READ ARTICLE
+              </Link>
+            )
+          }
         </div>
       </div>
     );
